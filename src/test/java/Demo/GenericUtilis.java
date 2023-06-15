2eb416ae6980b4d3581bf520984d5b783b167e22
Lenovo <Lenovo@192.168.1.5>
2023-06-15 14:53:53 +0530

    GenericUtils is commited

---
+++ src/test/java/Demo/GenericUtilis.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class GenericUtilis {
+
+public static void main(String[] args) {
+	GenericUtilis gu= new GenericUtilis();
+	gu.generic();
+}
+public void generic() {
+	System.out.println("I am generic");
+}
+	
+}
